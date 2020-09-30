6f84c4d92e75cce7fbeeba6efa94ad08535ccb23
Alexandru Ardelean <ardeleanalex@gmail.com>
2020-09-30 16:57:09 +0300

    src/pages/Index.vue: add basic dialog for adding a new device
    Will display a dialog-prompt to input the IP address for a new device in a
    list.
    Right now, just a simple text field is presented to input the IP address,
    however more validation will be done later.
    
    And maybe an input field for user-name & password.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,11 +4,30 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
     >
+
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+       <q-btn fab-mini color="green-8" icon="add" class="animate-pop" @click="newDevicePopup" />
+    </q-page-sticky>
+
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  methods: {
+    newDevicePopup () {
+      this.$q.dialog({
+        title: 'Add new device',
+        message: 'Enter IP address for device.',
+        prompt: {
+          model: '',
+          type: 'text'
+        },
+        cancel: true,
+        color: 'secondary'
+      })
+    }
+  }
 }
 </script>
